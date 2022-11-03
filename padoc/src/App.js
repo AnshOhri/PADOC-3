dc6f60013b05d157eb1e9dd408ca3fb4a869c82c
Nitin0000 <113214474+Nitin0000@users.noreply.github.com>
2022-11-03 17:53:29 +0530

    Add files via upload

---
+++ padoc/src/App.js
@@ -0,0 +1,21 @@
+import Signup from './components/signup'
+import './App.css';
+
+function App() {
+  return (
+    <div className='home'>
+      <div className='front_page'>
+        <h1>Padoc</h1>
+        <h3>
+          May 29, 2022 - Wikipedia is a free online encyclopedia, created and edited by volunteers around the world and hosted by the Wikimedia Foundation.
+        </h3>
+        <img src="padoc.png"></img>
+      </div>
+      <div className="App">
+        <Signup/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
